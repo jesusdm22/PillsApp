07f5f61428d5e794c656c084081ee130b1fa1187
jesusdm22 <jesusdm22@hotmail.com>
2019-11-25 14:47:19 +0100

    Calendario actualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { DetallesComponent } from './detalles/detalles.component';
 import { AgregarMedicamentoComponent } from './agregar-medicamento/agregar-medicamento.component';
 import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
+import { AplicacionComponent } from './aplicacion/aplicacion.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { LoginComponent } from './login/login.component';
     DetallesComponent,
     AgregarMedicamentoComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    AplicacionComponent
   ],
   imports: [
     BrowserModule
