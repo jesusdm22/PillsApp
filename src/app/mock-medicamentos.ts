88c65c274884cc1e0bb73e5b2cdef0b78e4bcab7
Ccablez <contactocristiantrappie@gmail.com>
2019-12-05 13:47:32 +0100

    Arreglos menores

--- src/app/mock-medicamentos.ts
+++ src/app/mock-medicamentos.ts
@@ -3,6 +3,6 @@ import {Medicamento} from './Medicamento';
 export const MEDICAMENTOS: Medicamento[] = [
   { id: 1, nombre: 'Paracetamol', tipo: 'capsules' , dosis: 2, fechaHora: 'XX/XX/XX 21:00h', descripcion: 'Descripcion de prueba' },
   { id: 2, nombre: 'Ibuprofeno', tipo: 'capsules', dosis: 1, fechaHora: 'XX/XX/XX 12:00h', descripcion: 'Descripcion de prueba' },
-  { id: 3, nombre: 'Dalsi', tipo: 'syringe', dosis: 2, fechaHora: 'XX/XX/XX 17:30h', descripcion: 'Descripcion de prueba' },
+  { id: 3, nombre: 'Dalsy', tipo: 'syringe', dosis: 2, fechaHora: 'XX/XX/XX 17:30h', descripcion: 'Descripcion de prueba' },
   { id: 4, nombre: 'Frenadol', tipo: 'tablets', dosis: 2, fechaHora: 'XX/XX/XX 22:15h', descripcion: 'Descripcion de prueba' }
 ];
